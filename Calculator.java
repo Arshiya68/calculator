3eef13888f210e7798e5b8092baf301ff4641d32
Arshiya68 <arshiyakhatun22@gmail.com>
2026-02-19 09:46:38 +0530

    Refactor output format in Calculator class

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,6 @@
 import java.util.*;
-    public class Calculator {
+
+public class Calculator {
     public static void main(String[] args) {
 
         if (args.length != 2) {
@@ -10,9 +11,9 @@ import java.util.*;
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println((double)a / b);
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + ((double) a / b));
     }
 }
